1c30609fa0b9c9bfc6d6978a9a52d6f775341fc8
Rony Rizk <rony_rizk@live.com>
2024-12-09 19:44:32 +0200

    c1

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3030;
+
+
+app.get('/',(req,res)=>{
+    res.send('Rony Rizk')
+})
+
+app.get('/students',(req,res)=>{
+    let data = [
+            {id:1, name:'Perla'},
+            {id:2, name:'Osman'},
+            {id:3, name:'Osman'}
+    ];
+    res.send(data)
+})
+
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});
